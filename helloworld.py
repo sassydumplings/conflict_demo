c76c89f6bb0bb91f48538cc5090be5a17220ae56
patsymprice <patsy.m.price@gmail.com>
2022-04-28 09:59:21 -0700

    fixed typos in helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('Naples",'Idaho')
+    hello_world('Naples','Idaho')
 
 
 main()
